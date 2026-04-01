b379295bdc25bdf8417ec8b45d7d87df6dcfe5c4
Li Ao <97588453+qwerbv@users.noreply.github.com>
2026-04-01 15:40:42 +0800

    Update plot.py

--- Downstream/plot.py
+++ Downstream/plot.py
@@ -7,25 +7,17 @@ from scipy import sparse
 from dataset import SpatialDataManager
 
 model_path = "BCA_pre"
-section_id = "E1"
-# show_gene = ["CALM2","CALM3","GFAP","ENC1","CKB"]  #151507
-# show_gene = ["Cryab","Rgs9","Tmeff2","Ptprn"]  #MB_HD
-# show_gene = ["Slc17a7", "Nrn1", "Hpca"]  # MB_HD
-# show_gene = ["PNMT","PIP","TSKU","CLDN4"]  #HBC_HD
-# show_gene = ["GPC3","VTN","AKR1C3"]    #HL_X
+section_id = "151507"
 
-# show_gene = ["ADH4","CFB","GATM"]     #HL_X2
-# show_gene = ["COX5B","COX6B1","TPI1","GUK1","MZT2B"]
-# show_gene = ["PARK7","LZIC","RBP7","EXOSC10","SDHB"]
 show_gene = ["GRB7","VIM","COL1A2","EMILIN1"]
 
 #绘制ground_truth
-# data_manager = SpatialDataManager(selection_id =section_id, train_ratio=0.5, seed=42, neighbor_ratio=4)
-# gt_counts = np.log2(data_manager.count_mtx.loc[:, data_manager.selected_genes].values+1)
-# all_coords = data_manager.in_tissue_coords
-# adata_gt = sc.AnnData(gt_counts)
-# adata_gt.obsm["coord"] = all_coords
-# adata_gt.var_names = data_manager.selected_genes
+data_manager = SpatialDataManager(selection_id =section_id, train_ratio=0.5, seed=42, neighbor_ratio=4)
+gt_counts = np.log2(data_manager.count_mtx.loc[:, data_manager.selected_genes].values+1)
+all_coords = data_manager.in_tissue_coords
+adata_gt = sc.AnnData(gt_counts)
+adata_gt.obsm["coord"] = all_coords
+adata_gt.var_names = data_manager.selected_genes
 
 # 绘制预测结果
 # adata_sample = sc.read_h5ad(f"{model_path}/{section_id}_final_adata.h5ad")
@@ -55,7 +47,7 @@ sc.pl.embedding(adata_sample,
 #                 save=f"{section_id}_{model_path}.png",
 #                 )
 #Xneium分辨率画图
-# sc.set_figure_params(dpi=400, figsize=(6.2, 3))
+# sc.set_figure_params(dpi=300, figsize=(6.2, 3))
 # sc.pl.embedding(adata_sample,
 #                 basis="coord",
 #                 color=show_gene,
@@ -64,7 +56,7 @@ sc.pl.embedding(adata_sample,
 #                 save=f"{section_id}_{model_path}.png",
 #                 )
 
-# sc.set_figure_params(dpi=400, figsize=(5.8, 4))
+# sc.set_figure_params(dpi=300, figsize=(5.8, 4))
 # sc.pl.embedding(adata_sample,
 #                 basis="coord",
 #                 color=show_gene,
